0a49adf21b78edcf7010fbfca23502dcfd5316cf
John Serrano <andrey_201015@hotmail.com>
2020-01-21 15:17:39 -0500

    Entendiendo los middlewares de error

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const app = express()
 
 const { config } = require('./config/index')
 const moviesApi = require('./routes/movies')
+const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers')
 
 // body-parser
 app.use(express.json())
 
 moviesApi(app)
+
+app.use(logErrors)
+app.use(errorHandler)
 
 app.listen(config.port, function () {
   console.log(`Listening http://localhost:${config.port}`)
