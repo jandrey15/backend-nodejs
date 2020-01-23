24785e25b4400f31ebfc93bdf52b60dd01f6de8a
John Serrano <andrey_201015@hotmail.com>
2020-01-23 10:42:28 -0500

    Add cache control to the routes

--- routes/movies.js
+++ routes/movies.js
@@ -8,6 +8,8 @@ const {
 } = require('../utils/schemas/movies')
 
 const validationHandler = require('../utils/middleware/validationHandler')
+const cacheResponse = require('../utils/cacheReponse')
+const { FIVE_MINUTES_IN_SECONDS, SIXTY_MINUTES_IN_SECONDS } = require('../utils/time')
 
 function moviesApi (app) {
   const router = express.Router()
@@ -16,7 +18,9 @@ function moviesApi (app) {
   const moviesService = new MoviesService()
 
   router.get('/', async function (req, res, next) {
+    cacheResponse(res, FIVE_MINUTES_IN_SECONDS)
     const { tags } = req.query
+
     try {
       const movies = await moviesService.getMovies({ tags })
 
@@ -30,6 +34,8 @@ function moviesApi (app) {
   })
 
   router.get('/:movieId', validationHandler({ movieId: movieIdSchema }, 'params'), async function (req, res, next) {
+    cacheResponse(res, SIXTY_MINUTES_IN_SECONDS)
+
     const { movieId } = req.params
 
     try {
